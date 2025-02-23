affbe30e1c9f1dff352eaac0ab594403a94784ac
WPI Student <vivekkasireddyr@gmail.com>
2025-02-23 09:22:04 -0500

    finished the todo for the pagetable.c

--- pagetable.c
+++ pagetable.c
@@ -36,7 +36,18 @@ void PT_SetPTE(int pid, int VPN, int PFN, int valid, int protection, int present
 	char* physmem = Memsim_GetPhysMem();
 	assert(PT_PageTableExists(pid)); // page table should exist if this is being called
 
-	//todo 
+	int pt_start = PT_GetRootPtrRegVal(pid); // Get the physical address where the page table starts
+
+    int entry_offset = VPN * 3; // Each PTE is 3 bytes long (1st byte: flags, 2nd: VPN, 3rd: PFN)
+    int entry_addr = pt_start + entry_offset;
+
+    // Construct the first byte with flags: Valid (bit 0), Protection (bit 1), Present (bit 2), Referenced (bit 3)
+    char flags = (valid & 0x1) | ((protection & 0x1) << 1) | ((present & 0x1) << 2) | ((referenced & 0x1) << 3);
+
+    // Write the PTE to memory
+    physmem[entry_addr] = flags;
+    physmem[entry_addr + 1] = (char)VPN;
+    physmem[entry_addr + 2] = (char)PFN;
 }
 
 /* 
@@ -49,17 +60,19 @@ void PT_SetPTE(int pid, int VPN, int PFN, int valid, int protection, int present
 int PT_PageTableInit(int pid, int pa){
 	char* physmem = Memsim_GetPhysMem();
 
-	// todo 
-	// zero out the page we are about to use for the page table 
+	// Check if there is a free page available; if not, evict one
+    if (pa == -1) {
+        pa = PT_Evict();  // Evict a page and use the newly freed space
+    }
 
-	// set the page table's root pointer register value
+    // Zero out the page we are about to use for the page table
+    memset(&physmem[pa], 0, PAGE_SIZE);
 
-	// return the PA of the next free page
-	
-	// If there were no free pages,
-	// Evict one and use the new space
-	
-	return pa;
+    // Set the page table's root pointer register value
+    PT_SetPTE(pid, 0, pa, 1, 0, 1, 0);  // Initialize the first entry of the page table
+
+    // Return the PA of the next free page
+    return pa + PAGE_SIZE;  // Assuming pages are contiguous
  }
 
 /* 
@@ -67,7 +80,10 @@ int PT_PageTableInit(int pid, int pa){
  * Returns true (non-zero) or false (zero).
  */
  int PT_PageTableExists(int pid){
- 	return 0; //todo 
+ 	if (pid < 0 || pid >= NUM_PROCESSES) {
+        return 0; // Invalid process ID
+    }
+    return PT_GetRootPtrRegVal(pid) != -1; // Assuming -1 means no page table assigned 
  }
 
 /* 
@@ -77,13 +93,14 @@ int PT_PageTableInit(int pid, int pa){
  * Finally, returns the starting physical address of the page table.
  */
 int PT_GetRootPtrRegVal(int pid){
-	//todo
-	// If the page table does not exist, return -1
+	// Check if the page table exists
+    if (!PT_PageTableExists(pid)) {
+        return -1; // Page table does not exist
+    }
+    // If the page table is not in memory, evict a page to bring it into memory
+    int rootPA = PT_PageTableInit(pid, PT_Evict());
 
-	// If the page table is not in memory, swap it in
-
-	// Return the starting physical address of the page table
-	return -1; 
+    return rootPA;
 }
 
 /*
@@ -95,12 +112,19 @@ int PT_GetRootPtrRegVal(int pid){
  * or another fair algorithm.
  */
 int PT_Evict() {
-	char* physmem = Memsim_GetPhysMem();
-	FILE* swapFile = MMU_GetSwapFileHandle();
+    static int nextEvictIndex = 0; // Keeps track of the next page to evict (round-robin)
+    char* physmem = Memsim_GetPhysMem();
+    FILE* swapFile = MMU_GetSwapFileHandle();
 
-	//todo
+    if (!physmem || !swapFile) {
+        return -1; // Error handling: Unable to access physical memory or swap file
+    }
 
-	return 0; //todo
+    // Identify the page to evict
+    int evictPage = nextEvictIndex;
+    nextEvictIndex = (nextEvictIndex + 1) % NUM_PAGES; // Move to the next page in a circular manner
+
+    return PAGE_START(evictPage); // Return the physical address of the evicted page
 }
 
 /*
@@ -112,12 +136,19 @@ int PT_Evict() {
  * 
  * Otherwise, returns -1.
  */
-int PT_VPNtoPA(int pid, int VPN){
-	char *physmem = Memsim_GetPhysMem();
+int PT_VPNtoPA(int pid, int VPN) {
+    if (!PT_PageTableExists(pid)) {
+        return -1;  // The process does not have a page table
+    }
 
-	//todo 
+    int rootAddress = PT_GetRootPtrRegVal(pid);  // Get page table base address
+    int PA = *(int*)(Memsim_GetPhysMem() + rootAddress + (VPN * sizeof(int)));  // Get PA from page table
 
-	return -1;
+    if (PA == -1) {
+        return -1;  // Page not found
+    }
+
+    return PA;  // Return the physical address
 }
 
 /*
@@ -125,10 +156,16 @@ int PT_VPNtoPA(int pid, int VPN){
  * to see if the protection bit is set to 1 (readable and writable).
  * If it is 1, it returns TRUE, and FALSE if it is not found or is 0.
  */
-int PT_PIDHasWritePerm(int pid, int VPN){
-	char* physmem = Memsim_GetPhysMem();
+int PT_PIDHasWritePerm(int pid, int VPN) {
+    if (!PT_PageTableExists(pid)) {
+        return FALSE;  // Process does not have a page table
+    }
 
-	return FALSE; //todo
+    int rootAddress = PT_GetRootPtrRegVal(pid);  // Get page table base address
+    int entryIndex = rootAddress + (VPN * sizeof(int) * 4); // Assuming each entry has multiple fields
+    int protectionBit = *(int*)(Memsim_GetPhysMem() + entryIndex + sizeof(int)); // Offset for protection bit
+
+    return (protectionBit == 1) ? TRUE : FALSE; // Return TRUE if writable, FALSE otherwise
 }
 
 /* 
@@ -136,6 +173,8 @@ int PT_PIDHasWritePerm(int pid, int VPN){
  * For example, -1 for the starting physical address of the page table, and FALSE for present.
  */
 void PT_Init() {
-
-	return; //todo
+	for (int pid = 0; pid < NUM_PROCESSES; pid++) {
+        // Initialize the page table for each process
+        PT_PageTableInit(pid, -1); // Set the initial physical address to -1 (not allocated)
+    }
 }
